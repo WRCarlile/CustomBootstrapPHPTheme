29c17cf99e3673810db464de3ba7192f3e19676e
“Ryan <ryancarlile@gmail.com>
2016-11-12 14:05:49 -0800

    Add header and icons

---
+++ bootstrap-portfolio/functions.php
@@ -0,0 +1,17 @@
+<?php
+	/*
+	# =================================================
+	# functions.php
+	#
+	# The theme's functions.
+	# =================================================
+	*/
+
+	/* ------------------------------------------------ */
+	/* 1. CONSTANTS */
+	/* ------------------------------------------------ */
+	define( 'THEMEROOT', get_stylesheet_directory_uri() );
+	define( 'IMAGES', THEMEROOT . '/img' );
+	define( 'JS', THEMEROOT . '/js' );
+	
+?>
