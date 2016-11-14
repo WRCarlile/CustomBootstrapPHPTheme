a4a3b9b4104e01a7483304f72736ed341b07c0a7
“Ryan <ryancarlile@gmail.com>
2016-11-14 13:58:34 -0800

    Add body and footer

--- bootstrap-portfolio/functions.php
+++ bootstrap-portfolio/functions.php
@@ -37,5 +37,49 @@ if ( ! function_exists('tuts_theme_setup')) {
 
     add_action( 'after_setup_theme', 'tuts_theme_setup' );
 }
+/* ------------------------------------------------ */
+/* 3. GET POST META */
+/* ------------------------------------------------ */
+if ( ! function_exists( 'tuts_post_meta' ) ) {
+    function tuts_post_meta() {
+        if ( get_post_type() === 'post' ) {
+            /* Post author. */
+            echo '<p class="post-meta">';
+            _e( 'by', 'tuts' );
+            printf( '<a href="%1$s" rel="author"> %2$s </a>', esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ), get_the_author() );
+            _e( 'on', 'tuts' );
+            echo ' <span>' . get_the_date() . '</span></p>';
+        }
+    }
+}
+/* ------------------------------------------------ */
+/* 4. NUMBERED PAGINATION */
+/* ------------------------------------------------ */
+if ( ! function_exists( 'tuts_numbered_pagination' ) ) {
+    function tuts_numbered_pagination() {
+        $args = array(
+            'prev_next' => false,
+            'type' => 'array'
+        );
+
+        echo '<div class="col-md-12">';
+        $pagination = paginate_links( $args );
+
+        if ( is_array( $pagination ) ) {
+            echo '<ul class="nav nav-pills">';
+            foreach ( $pagination as $page ) {
+                if ( strpos( $page, 'current' ) ) {
+                    echo '<li class="active"><a href="#">' . $page . '</a></li>';
+                } else {
+                    echo '<li>' . $page . '</li>';
+                }
+            }
+
+            echo '</ul>';
+        }
+
+        echo '</div>';
+    }
+}
 
 ?>
